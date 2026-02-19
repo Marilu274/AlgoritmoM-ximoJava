7662dff9418beff9641f0b726f0f7e9fc0f5bd88
Marilu274 <marilu60274@gmail.com>
2026-02-18 18:20:38 -0600

    se agrego metodo mein

---
+++ Ordenamiento.java
@@ -0,0 +1,8 @@
+package Evaluación;
+
+public class Ordenamiento {
+public static void main(String[] args) {
+ System.out.println("¡Hola Mundo!");
+ System.out.println("Mi primer programa en Java");
+ }
+ }
